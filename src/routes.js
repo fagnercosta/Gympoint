dafa6abd70c7a697992a88f7ef08a43186cd560c
root <!gown119>
2019-11-02 11:51:36 -0400

    Autenticacao de Usuários

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import StudentController from './app/controllers/StudentController';
 import SessionController from './app/controllers/SessionController';
+import authMidleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/students', StudentController.store);
 
 routes.post('/sessions', SessionController.store);
+
+routes.post('/teste', authMidleware, (req, res) => {
+  return res.json({ error: 'Deu certo' });
+});
 
 export default routes;
